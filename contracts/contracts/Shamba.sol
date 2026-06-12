f817083997e4fda3d3f6ebae7b2c1805a51ae354
victor-olamide <olaboyevictor20@gmail.com>
2026-06-12 15:48:00 +0100

    Shamba.sol: add CropState enum, Plot and Farm structs

--- contracts/contracts/Shamba.sol
+++ contracts/contracts/Shamba.sol
@@ -9,16 +9,29 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Shamba is Ownable, ReentrancyGuard {
     IERC20 public immutable usdm;
 
-    // Crop types: 0=Maize, 1=Tomato, 2=Cassava, 3=Sunflower, 4=GoldenWheat(premium)
     uint8 public constant CROP_COUNT = 5;
     uint8 public constant MAX_PLOTS  = 6;
 
-    // Growth times in seconds
-    uint32[5] public growthTime = [1 hours, 2 hours, 4 hours, 6 hours, 30 minutes];
-    // Harvest yield in score points
+    uint32[5] public growthTime   = [1 hours, 2 hours, 4 hours, 6 hours, 30 minutes];
     uint256[5] public harvestYield = [10, 20, 40, 60, 100];
-    // Seed cost in USDM (0 = free)
-    uint256[5] public seedCost = [0, 0, 0, 0, 0.05 ether];
+    uint256[5] public seedCost    = [0, 0, 0, 0, 0.05 ether];
+
+    enum CropState { EMPTY, PLANTED, READY }
+
+    struct Plot {
+        uint8 cropType;
+        uint32 plantedAt;
+        bool watered;
+        CropState state;
+    }
+
+    struct Farm {
+        Plot[6] plots;
+        uint32 totalHarvests;
+        uint256 score;
+        uint256 lastVisit;
+        bool initialized;
+    }
 
     constructor(address _usdm) Ownable(msg.sender) {
         usdm = IERC20(_usdm);
