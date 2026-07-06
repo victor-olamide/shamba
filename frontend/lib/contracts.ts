a10d96387cb68a1a281f435c313b860bea11c506
victor-olamide <olaboyevictor20@gmail.com>
2026-07-06 23:08:25 +0100

    frontend: add balanceOf to ERC20_ABI for USDM balance checks

--- frontend/lib/contracts.ts
+++ frontend/lib/contracts.ts
@@ -36,5 +36,6 @@ export const SHAMBA_ABI = [
 ] as const;
 
 export const ERC20_ABI = [
-  { name: "approve", type: "function", stateMutability: "nonpayable", inputs: [{ name: "spender", type: "address" }, { name: "amount", type: "uint256" }], outputs: [{ name: "", type: "bool" }] },
+  { name: "approve",    type: "function", stateMutability: "nonpayable", inputs: [{ name: "spender", type: "address" }, { name: "amount", type: "uint256" }], outputs: [{ name: "", type: "bool" }] },
+  { name: "balanceOf",  type: "function", stateMutability: "view",       inputs: [{ name: "account", type: "address" }], outputs: [{ name: "", type: "uint256" }] },
 ] as const;
